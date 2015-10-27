b45554651b79afaf5f275abeb911f740076c3257
ferris <jake@fusetools.com>
2015-10-26 18:45:39 -0700

    added bottom bar

--- api-keys.js
+++ api-keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-	appId: "YOUR APP ID HERE",
-	jsKey: "YOUR JS KEY HERE"
+	appId: "JrNKKJ3xswaUn670aB5KUQBo1QdTzWYExXyKSWv0",
+	jsKey: "Xa6O8PywPz0Th5nvTFI0952Mvceb8aqqK1zUiE1R"
 };
