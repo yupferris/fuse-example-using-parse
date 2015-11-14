1b324aa8a9e9dc69966fe094dc85bb664f159f29
ferris <jake@outracks.com>
2015-11-13 16:21:05 -0800

    Removed API keys
    Whoops :) App is deleted anyways.

--- api-keys.js
+++ api-keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-	appId: "JrNKKJ3xswaUn670aB5KUQBo1QdTzWYExXyKSWv0",
-	jsKey: "Xa6O8PywPz0Th5nvTFI0952Mvceb8aqqK1zUiE1R"
+	appId: "YOUR APP ID HERE",
+	jsKey: "YOUR JS KEY HERE"
 };
